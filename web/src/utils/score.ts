0e850670e650ff69f5fd01df23f5f420989149f5
Keyi Li <23731267+keyi6@users.noreply.github.com>
2022-08-02 23:55:11 +0800

    fix: daily limit not working

--- web/src/utils/score.ts
+++ web/src/utils/score.ts
@@ -5,7 +5,7 @@ import { ISubmission } from '../common/data_center/services';
 export const DAILY_LIMIT = 800;
 
 export function calcDailyScore(dailySubmissions: ISubmission[]): number {
-    return [
+    return Math.min(DAILY_LIMIT, [
         { difficulty: null, score: 80 },
         { difficulty: Difficulty.EASY, score: 80 },
         { difficulty: Difficulty.MEDIUM, score: 100 },
@@ -16,5 +16,5 @@ export function calcDailyScore(dailySubmissions: ISubmission[]): number {
             .map(s => s.titleSlug)
         ).length * score
     )
-    .reduce((prev, cur) => prev + cur, 0);
+    .reduce((prev, cur) => prev + cur, 0));
 }
